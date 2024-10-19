bca0d7f0e5c0c1b3e6ab148586412f950c5be838
Ahmad Qamar <example@example.com>
2024-10-20 04:41:52 +0500

    Setting puppeteer up to visit ray.so

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-123
+import puppeteer from 'puppeteer';
+
+const RAY_SO_URL = "https://ray.so/";
+
+const testing = true;
+console.log("Starting the browser...");
+const browser = await puppeteer.launch({headless: !testing});
+console.log("Browser has been initialized");
+const page = await browser.newPage();
+await page.goto(RAY_SO_URL);
+
+await new Promise(r => setTimeout(r, 5000));
+
+
