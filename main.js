822bb5492c502823da2770a390aa2ae24729a4c9
Ahmad Qamar <example@example.com>
2024-10-20 05:35:09 +0500

    Logic to locally run the ray.so server to cater for situations where internet isn't available

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 import puppeteer from 'puppeteer';
+import fs from 'fs';
+import { exec, spawn, fork } from 'child_process';
 
-const RAY_SO_URL = "https://ray.so/";
+const runLocally = true;
+
+var serverProcess = null;
+const cleanup = () => {
+    if (serverProcess != null) serverProcess.kill();
+    process.exit();
+};
+
+process.on('SIGINT', cleanup);
+process.on('SIGTERM', cleanup);
+
+const port = 18432;
+if (runLocally) {
+    const raySoProjectPath = process.env.RaySoPath;
+
+    if (!fs.existsSync(raySoProjectPath)) {
+        console.error("Please set the 'RaySoPath' environment variable before proceeding.");
+        process.exit();
+    }
+
+    serverProcess = spawn(`npm run dev -- "-p ${port}"`, { cwd: raySoProjectPath, stdio: 'inherit', shell: true });
+}
+const RAY_SO_URL = runLocally ? `http://localhost:${port}` : "https://ray.so/";
 
 const testing = true;
 console.log("Starting the browser...");
-const browser = await puppeteer.launch({headless: !testing});
+const browser = await puppeteer.launch({ headless: !testing });
 console.log("Browser has been initialized");
 const page = await browser.newPage();
 await page.goto(RAY_SO_URL);
