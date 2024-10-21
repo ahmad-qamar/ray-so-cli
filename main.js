ca31ee078deb22ee8d73dcd78134e5bc53895378
Ahmad Qamar <example@example.com>
2024-10-22 03:05:08 +0500

    Updated code to click the copy button, and includes methods to intercept the image being copied to the clipboard, as well as receiving it back as a string.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import puppeteer from 'puppeteer';
 import fs from 'fs';
 import { exec, spawn, fork } from 'child_process';
-
-const runLocally = true;
+import { tmpdir } from 'os';
+const runLocally = false;
 
 var serverProcess = null;
 const cleanup = () => {
@@ -23,16 +23,38 @@ if (runLocally) {
     }
 
     serverProcess = spawn(`npm run dev -- "-p ${port}"`, { cwd: raySoProjectPath, stdio: 'inherit', shell: true });
+    await new Promise(r => setTimeout(r, 7000));
 }
 const RAY_SO_URL = runLocally ? `http://localhost:${port}` : "https://ray.so/";
-
+const BUTTON1_LOCATOR = '#radix-\\:R2r7rqfiba\\:';
+const BUTTON2_LOCATOR = 'div ::-p-text( Copy Image)';
 const testing = true;
+
 console.log("Starting the browser...");
 const browser = await puppeteer.launch({ headless: !testing });
+const context = browser.defaultBrowserContext();
+await context.overridePermissions(RAY_SO_URL, ['clipboard-read']);
 console.log("Browser has been initialized");
 const page = await browser.newPage();
 await page.goto(RAY_SO_URL);
+await new Promise(r => setTimeout(r, 3000));
+await page.evaluate(() => {
+    navigator.clipboard.write = async function (data) {
+        var img  = await data[0].getType("image/png");
+        var imgStr = await img.text();
 
-await new Promise(r => setTimeout(r, 5000));
+        window.imgData = imgStr;
+    };
+});
+
+var element = await page.$(BUTTON1_LOCATOR);
+await element.click();
+element = await page.$(BUTTON2_LOCATOR);
+await element.click();
+await new Promise(r => setTimeout(r, 1500));
+
+var img = await page.evaluate("window.imgData");
+
+process.exit();
 
 
